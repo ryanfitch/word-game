4fc8d6daf36ffb6fc65104467d9459fd9f2267c5
Kit <chocorexic@gmail.com>
2015-05-26 23:20:38 +0100

    Got basic game behaviour working
    Program terminates properly after all letters have been acquired. To
    implement limited guesses and other stuff next.

--- word_game.py
+++ word_game.py
@@ -3,36 +3,49 @@ lives = 3
 characters = []
 game_output = []
 
+win = False
+
 
 def get_word():
+  # eventually get a random word from a list of words
   return word_list[0]
 
 def split_word(_word):
   return list(_word)
 
-def get_length(_characters):
-  return len(_characters)
-
 def create_game(list_of_characters):
-  new_game = '_ ' * len(list_of_characters)
+  new_game = ['_ '] * len(list_of_characters)
   return new_game
 
-#def check_characters(_input):
-#  index = 0
-#  for i in characters:
-#    if i == _input:
+def check_guess(_input):
+
+  for i, letter in enumerate(characters):
+    if _input == letter:
+      game_output[i] = _input
+
+def check_game_state():
+  score = 0
+  for i, letter in enumerate(characters):
+    if game_output[i] == letter:
+      score += 1
+  if score == len(characters):
+    return True
+  else:
+    return False
+
 
 characters = split_word(get_word())
 game_output = create_game(characters)
+
 print("Make a guess: ")
 print(game_output)
 
-while lives > 0:
+while win == False:
+  _input = input()
+  check_guess(_input)
+  print(game_output)
+  win = check_game_state()
 
 
-  # print("make a guess")
-  # print(": ")
-
-  # print (get_length(guess))
 
 
